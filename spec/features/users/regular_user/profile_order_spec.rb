1693203358f6d7533af904c4c104ba097932f2c2
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-06 18:16:53 -0600

    Updates user profile order tests to include cancel link
    Co-authored-by: Tyler Schaffer 
    <48742436+tschaffer1618@users.noreply.github.com>
    Co-authored-by: Nancy Lee 
    <24424825+nancylee713@users.noreply.github.com>
    Co-authored-by: Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>
    
    Co-Authored-By: null <tschaffer1618@users.noreply.github.com>
    Co-Authored-By: Nancy Lee <nancylee713@users.noreply.github.com>
    Co-Authored-By: Mack Halliday <MackHalliday@users.noreply.github.com>

--- spec/features/users/regular_user/profile_order_spec.rb
+++ spec/features/users/regular_user/profile_order_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe "User Profile Order Page" do
     @item_2 = merchant_1.items.create!(attributes_for(:item))
 
     @order_1 = create(:order)
+    @order_2 = create(:order, status: 'packaged')
     @item_order_1 = @user.item_orders.create!(order: @order_1, item: @item_1, quantity: 1, price: @item_1.price)
     @item_order_2 = @user.item_orders.create!(order: @order_1, item: @item_2, quantity: 3, price: @item_2.price)
   end
@@ -89,5 +90,25 @@ RSpec.describe "User Profile Order Page" do
       expect(page).to have_content(@item_order_1.updated_at)
       expect(page).to have_content(@item_order_1.order.status)
     end
+  end
+
+  it "I can cancel the order only if it's pending" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    visit "/profile/orders/#{@order_1.id}"
+
+    expect(page).to have_link("Cancel Order")
+
+    click_link "Cancel Order"
+
+    expect(current_path).to eq(profile_path)
+    expect(page).to have_content("Your order has been cancelled")
+
+    visit "/profile/orders/#{@order_1.id}"
+
+    expect(page).to have_content("cancelled")
+
+    visit "/profile/orders/#{@order_2.id}"
+
+    expect(page).not_to have_link("Cancel Order")
   end
 end
