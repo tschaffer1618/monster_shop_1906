cff9a155853b365b8659e79e81b2c96987e85817
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-06 18:16:53 -0600

    Removes the cancel order test from order show page
    Co-authored-by: Tyler Schaffer 
    <48742436+tschaffer1618@users.noreply.github.com>
    Co-authored-by: Nancy Lee 
    <24424825+nancylee713@users.noreply.github.com>
    Co-authored-by: Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>
    
    Co-Authored-By: null <tschaffer1618@users.noreply.github.com>
    Co-Authored-By: Nancy Lee <nancylee713@users.noreply.github.com>
    Co-Authored-By: Mack Halliday <MackHalliday@users.noreply.github.com>

--- spec/features/orders/show_spec.rb
+++ spec/features/orders/show_spec.rb
@@ -14,28 +14,7 @@ RSpec.describe "User Profile Order Page" do
 
     @item_order_1 = @user.item_orders.create!(order: @order_1, item: @item_1, quantity: 1, price: @item_1.price)
     @item_order_2 = @user.item_orders.create!(order: @order_1, item: @item_2, quantity: 3, price: @item_2.price)
-  end
-
-  it "I can cancel the order only if it's pending" do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-    visit "/orders/#{@order_1.id}"
-
-    expect(page).to have_link("Cancel Order")
-
-    click_link "Cancel Order"
-
-    expect(current_path).to eq(profile_path)
-    expect(page).to have_content("Your order has been cancelled")
-
-    visit "/orders/#{@order_1.id}"
-
-    expect(page).to have_content("Unfulfilled")
-    expect(page).to have_content("cancelled")
-
-    visit "/orders/#{@order_2.id}"
-
-    expect(page).not_to have_link("Cancel Order")
-  end
+  end 
 
   it "status changes from 'pending' to 'packaged' once all items are fulfilled" do
     michael = User.create!(name: "Michael Scott",
