6357de2177531a4d75c1e6abafe7154ba9a6a642
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-06 18:16:53 -0600

    Creates tests for top/bottom 5 items
    Co-authored-by: Tyler Schaffer 
    <48742436+tschaffer1618@users.noreply.github.com>
    Co-authored-by: Nancy Lee 
    <24424825+nancylee713@users.noreply.github.com>
    Co-authored-by: Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>
    
    Co-Authored-By: null <tschaffer1618@users.noreply.github.com>
    Co-Authored-By: Nancy Lee <nancylee713@users.noreply.github.com>
    Co-Authored-By: Mack Halliday <MackHalliday@users.noreply.github.com>

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -19,8 +19,6 @@ RSpec.describe "Items Index Page" do
       expect(page).to have_link(@tire.merchant.name)
       expect(page).to have_link(@pull_toy.name)
       expect(page).to have_link(@pull_toy.merchant.name)
-      # expect(page).to have_link(@dog_bone.name)
-      # expect(page).to have_link(@dog_bone.merchant.name)
     end
 
     it "I can see a list of all of the items "do
@@ -56,8 +54,62 @@ RSpec.describe "Items Index Page" do
       expect(page).to_not have_content("Price: $#{@dog_bone.price}")
       expect(page).to_not have_content("Inactive")
       expect(page).to_not have_content("Inventory: #{@dog_bone.inventory}")
-      # expect(page).to_not have_link(@brian.name)
       expect(page).to_not have_css("img[src*='#{@dog_bone.image}']")
+    end
+
+    it "shows an area with statistics of top/bottom 5 most/least popular items (by quantity purchased), plus that quantity" do
+      shop = create(:merchant)
+      item_1 = shop.items.create(attributes_for(:item, name: "apple"))
+      item_2 = shop.items.create(attributes_for(:item, name: "orange"))
+      item_3 = shop.items.create(attributes_for(:item, name: "banana"))
+      item_4 = shop.items.create(attributes_for(:item, name: "pear"))
+      item_5 = shop.items.create(attributes_for(:item, name: "lychee"))
+      item_6 = shop.items.create(attributes_for(:item, name: "watermelon"))
+
+      user = create(:user)
+      order_1 = create(:order)
+      item_order_1 = user.item_orders.create!(order: order_1, item: item_6, quantity: 6, price: item_6.price)
+      item_order_2 = user.item_orders.create!(order: order_1, item: item_5, quantity: 5, price: item_5.price)
+      item_order_3 = user.item_orders.create!(order: order_1, item: item_4, quantity: 4, price: item_4.price)
+      item_order_4 = user.item_orders.create!(order: order_1, item: item_3, quantity: 3, price: item_3.price)
+
+      order_2 = create(:order)
+      item_order_5 = user.item_orders.create!(order: order_2, item: item_6, quantity: 6, price: item_6.price)
+      item_order_6 = user.item_orders.create!(order: order_2, item: item_1, quantity: 1, price: item_1.price)
+      item_order_7 = user.item_orders.create!(order: order_2, item: item_2, quantity: 2, price: item_2.price)
+      item_order_8 = user.item_orders.create!(order: order_2, item: item_3, quantity: 3, price: item_3.price)
+
+      visit items_path
+
+      expect(page).to have_content("Item Statistics")
+      expect(page).to have_content("Top 5 Items")
+      expect(page).to have_content("Bottom 5 Items")
+
+      within "#top_five" do
+        expect(page).to have_content(item_6.name)
+        expect(page).to have_content(12)
+        expect(page).to have_content(item_3.name)
+        expect(page).to have_content(6)
+        expect(page).to have_content(item_5.name)
+        expect(page).to have_content(5)
+        expect(page).to have_content(item_4.name)
+        expect(page).to have_content(4)
+        expect(page).to have_content(item_2.name)
+        expect(page).to have_content(2)
+      end
+
+      within "#bottom_five" do
+        expect(page).to have_content(item_1.name)
+        expect(page).to have_content(1)
+        expect(page).to have_content(item_3.name)
+        expect(page).to have_content(6)
+        expect(page).to have_content(item_5.name)
+        expect(page).to have_content(5)
+        expect(page).to have_content(item_4.name)
+        expect(page).to have_content(4)
+        expect(page).to have_content(item_2.name)
+        expect(page).to have_content(2)
+      end
     end
   end
 end
