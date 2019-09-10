ee985bb0f13622fe1fd00304937b03b9b700b04d
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-10 08:09:59 -0600

    Update with comment about @orders

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Admin::DashboardController < Admin::BaseController
   def index
     @admin_user = current_user
+    #need to move into model?
     @orders = Order.order(status: :asc)
     @users = User.all
   end
