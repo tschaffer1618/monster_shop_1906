467a955a5298d07efa8208d651c42fb3dff045ac
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-09 20:41:44 -0600

    Add method to sort Orders by acs

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < Admin::BaseController
   def index
     @admin_user = current_user
-    @orders = Order.all
+    @orders = Order.order(status: :asc)
     @users = User.all
   end
 end
