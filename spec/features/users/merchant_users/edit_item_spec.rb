faa265d59157805ea0e0296ddaab679f5f619f33
Nancy Lee <srlee713@gmail.com>
2019-09-10 17:29:10 -0600

    Add spec tests for merchant editing an item
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
                    Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
                    Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- spec/features/users/merchant_users/edit_item_spec.rb
+++ spec/features/users/merchant_users/edit_item_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Merchant Items Page" do
   describe "As a merchant admin" do
     before :each do
       @shop = create(:merchant)
-      @item_1 = @shop.items.create(attributes_for(:item, name: "apple"))
+      @item_1 = @shop.items.create(name: "apple", description: "fresh", price: 10.00, image: "https://avatars3.githubusercontent.com/u/6475745?s=88&v=4", active?: true, inventory: 30)
       @item_2 = @shop.items.create(attributes_for(:item, name: "orange"))
 
       user = create(:user)
@@ -18,13 +18,23 @@ RSpec.describe "Merchant Items Page" do
 
     describe "I can edit an item" do
       it "when I click a link to edit an item, I see a form with all fields auto-filled" do
-
         within "#item-#{@item_1.id}" do
           expect(page).to have_link("Edit")
           click_link("Edit")
         end
 
-        expect(current_path).to eq(edit_merchant_user(@item_1))
+        expect(current_path).to eq(edit_merchant_user_path(@item_1))
+
+        expect(find_field('Name').value).to eq 'apple'
+        expect(find_field('Description').value).to eq 'fresh'
+        expect(find_field('Price').value).to eq '10'
+        expect(find_field('Image').value).to eq "https://avatars3.githubusercontent.com/u/6475745?s=88&v=4"
+        expect(find_field('Inventory').value).to eq '30'
+
+        click_on("Update Item")
+        expect(current_path).to eq(merchant_user_index_path)
+        expect(page).to have_content("Your item is now updated!")
+        expect(page).to have_css("img[src*='https://avatars3.githubusercontent.com/u/6475745?s=88&v=4']")
       end
     end
 
