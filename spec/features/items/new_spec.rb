97f9ccde2f55a81777608f3f55675f29f238dde8
Nancy Lee <srlee713@gmail.com>
2019-09-09 21:18:55 -0600

    Modify item spec tests so that only merchant admin can add new items
    Co-authored-by: Tyler Schaffer 
    <48742436+tschaffer1618@users.noreply.github.com>
                    Leiya Kenney 
    <45922590+leiyakenney@users.noreply.github.com>
                    Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>

--- spec/features/items/new_spec.rb
+++ spec/features/items/new_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe "Create Merchant Items" do
   describe "When I visit the merchant items index page" do
     before(:each) do
       @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+      merchant_admin = create(:user, role: 2, merchant: @brian)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_admin)
     end
 
     it 'I see a link to add a new item for that merchant' do
@@ -25,6 +27,7 @@ RSpec.describe "Create Merchant Items" do
 
       expect(page).to have_link(@brian.name)
       expect(current_path).to eq(new_merchant_item_path(@brian))
+
       fill_in :name, with: name
       fill_in :price, with: price
       fill_in :description, with: description
@@ -35,20 +38,21 @@ RSpec.describe "Create Merchant Items" do
 
       new_item = Item.last
 
-      expect(current_path).to eq(merchant_items_path(@brian))
+      expect(current_path).to eq(merchant_user_index_path)
       expect(new_item.name).to eq(name)
       expect(new_item.price).to eq(price)
       expect(new_item.description).to eq(description)
       expect(new_item.image).to eq(image_url)
       expect(new_item.inventory).to eq(inventory)
       expect(Item.last.active?).to be(true)
+
       expect("#item-#{Item.last.id}").to be_present
       expect(page).to have_content(name)
-      expect(page).to have_content("Price: $#{new_item.price}")
+      expect(page).to have_content("$#{new_item.price}")
       expect(page).to have_css("img[src*='#{new_item.image}']")
       expect(page).to have_content("Active")
-      expect(page).to_not have_content(new_item.description)
-      expect(page).to have_content("Inventory: #{new_item.inventory}")
+      expect(page).to have_content(new_item.description)
+      expect(page).to have_content("#{new_item.inventory}")
     end
 
     it 'I get an alert if I dont fully fill out the form' do
@@ -70,7 +74,7 @@ RSpec.describe "Create Merchant Items" do
 
       click_button "Create Item"
 
-      expect(page).to have_content("Name can't be blank and Inventory can't be blank")
+      expect(page).to have_content("Name can't be blank, Inventory can't be blank, and Inventory is not a number")
       expect(page).to have_button("Create Item")
     end
   end
