5cd82f7b431305857718e2c8feb7c2c0cfd197dc
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-10 14:49:32 -0600

    Update with update_status method

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,14 @@
 class Admin::DashboardController < Admin::BaseController
   def index
     @admin_user = current_user
-    #need to move into model?
     @orders = Order.order(status: :asc)
     @users = User.all
+  end
+
+  def update_status
+    order = Order.find(params[:order_id])
+    order.update(status: 2)
+    flash[:success] = "Order ##{order.id} has been shipped!"
+    redirect_to admin_path
   end
 end
