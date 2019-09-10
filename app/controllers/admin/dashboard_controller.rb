8d5ca8c0474c6af3b4515dcfb0a6bf791c0b8b71
tschaffer1618 <48742436+tschaffer1618@users.noreply.github.com>
2019-09-10 16:17:50 -0600

    Update dashboard_controller.rb

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,6 @@ class Admin::DashboardController < Admin::BaseController
   def index
     @admin_user = current_user
     @orders = Order.order(status: :asc)
-    @users = User.all
   end
 
   def update_status
