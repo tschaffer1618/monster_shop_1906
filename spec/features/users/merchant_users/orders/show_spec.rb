da6b877eb7afe4164eb006abd22210c69ffbe410
Tyler Schaffer <tpschaffer@gmail.com>
2019-09-14 11:04:56 -0600

    Fix merchant user order show tests

--- spec/features/users/merchant_users/orders/show_spec.rb
+++ spec/features/users/merchant_users/orders/show_spec.rb
@@ -3,29 +3,30 @@ require 'rails_helper'
 RSpec.describe "Merchant Order Show Page" do
   before :each do
     @regular_user_1 = create(:user)
+    @address_1 = @regular_user_1.addresses.create(name: @regular_user_1.name, street_address: @regular_user_1.address, city: @regular_user_1.city, state: @regular_user_1.state, zipcode: @regular_user_1.zipcode, nickname: 'home')
 
     @merchant_shop_1 = create(:merchant, name: "Merchant Shop 1")
-      @item_1 = @merchant_shop_1.items.create!(attributes_for(:item, name: "Item 1", inventory: 10))
-      @item_2 = @merchant_shop_1.items.create!(attributes_for(:item, name: "Item 2", inventory: 15))
-      @item_5 = @merchant_shop_1.items.create!(attributes_for(:item, name: "Item 5", inventory: 0))
+      @item_1 = @merchant_shop_1.items.create(attributes_for(:item, name: "Item 1", inventory: 10))
+      @item_2 = @merchant_shop_1.items.create(attributes_for(:item, name: "Item 2", inventory: 15))
+      @item_5 = @merchant_shop_1.items.create(attributes_for(:item, name: "Item 5", inventory: 0))
 
     @merchant_shop_2 = create(:merchant, name: "Merchant Shop 2")
-      @item_3 = @merchant_shop_2.items.create!(attributes_for(:item, name: "Item 3", inventory: 20))
-      @item_4 = @merchant_shop_2.items.create!(attributes_for(:item, name: "Item 4", inventory: 10))
+      @item_3 = @merchant_shop_2.items.create(attributes_for(:item, name: "Item 3", inventory: 20))
+      @item_4 = @merchant_shop_2.items.create(attributes_for(:item, name: "Item 4", inventory: 10))
 
-    @order_1 = create(:order, status: "pending")
-      @item_order_1 = @regular_user_1.item_orders.create!(order: @order_1, item: @item_1, quantity: 2, price: @item_1.price, user: @regular_user_1, fulfilled?: false)
-      @item_order_2 = @regular_user_1.item_orders.create!(order: @order_1, item: @item_2, quantity: 8, price: @item_2.price, user: @regular_user_1, fulfilled?: false)
-      @item_order_3 = @regular_user_1.item_orders.create!(order: @order_1, item: @item_3, quantity: 10, price: @item_3.price, user: @regular_user_1, fulfilled?: false)
+    @order_1 = @address_1.orders.create
+      @item_order_1 = @order_1.item_orders.create(item: @item_1, quantity: 2, price: @item_1.price, fulfilled?: false)
+      @item_order_2 = @order_1.item_orders.create(item: @item_2, quantity: 8, price: @item_2.price, fulfilled?: false)
+      @item_order_3 = @order_1.item_orders.create(item: @item_3, quantity: 10, price: @item_3.price, fulfilled?: false)
 
-    @order_2 = create(:order, status: "pending")
-      @item_order_4 = @regular_user_1.item_orders.create(order: @order_2, item: @item_2, quantity: 100, price: @item_2.price, user: @regular_user_1, fulfilled?: false)
+    @order_2 = @address_1.orders.create
+      @item_order_4 = @order_2.item_orders.create(item: @item_2, quantity: 100, price: @item_2.price, fulfilled?: false)
 
-    @order_3 = create(:order, status: "pending")
-      @item_order_5 = @regular_user_1.item_orders.create(order: @order_3, item: @item_4, quantity: 18, price: @item_4.price, user: @regular_user_1, fulfilled?: false)
+    @order_3 = @address_1.orders.create
+      @item_order_5 = @order_3.item_orders.create(item: @item_4, quantity: 18, price: @item_4.price, fulfilled?: false)
 
-    @order_4 = create(:order, status: "pending")
-      @item_order_6 = @regular_user_1.item_orders.create(order: @order_4, item: @item_5, quantity: 15, price: @item_5.price, user: @regular_user_1, fulfilled?: false)
+    @order_4 = @address_1.orders.create
+      @item_order_6 = @order_4.item_orders.create(item: @item_5, quantity: 15, price: @item_5.price, fulfilled?: false)
 
     @merchant_admin_1 = create(:user, role: 2, merchant: @merchant_shop_1)
     @merchant_employee_1 = create(:user, role: 1, merchant: @merchant_shop_1)
@@ -48,11 +49,11 @@ RSpec.describe "Merchant Order Show Page" do
     visit merchant_order_path(@order_1)
 
     within "#shipping-address" do
-      expect(page).to have_content(@order_1.name)
-      expect(page).to have_content(@order_1.address)
-      expect(page).to have_content(@order_1.city)
-      expect(page).to have_content(@order_1.state)
-      expect(page).to have_content(@order_1.zip)
+      expect(page).to have_content(@order_1.address.name)
+      expect(page).to have_content(@order_1.address.street_address)
+      expect(page).to have_content(@order_1.address.city)
+      expect(page).to have_content(@order_1.address.state)
+      expect(page).to have_content(@order_1.address.zipcode)
     end
 
     within "#item-orders-#{@item_order_1.id}" do
@@ -80,7 +81,7 @@ RSpec.describe "Merchant Order Show Page" do
 
     expect(page).not_to have_css("#item_orders-#{@item_order_4.id}")
   end
-  
+
   it 'merchant user cannot see the shipping information if not their order' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_admin_1)
 
